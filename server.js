6494cc720c4ed94638af4b41c279407e8c8fa5ad
kishoreahmed <kishoreahmed27@gmail.com>
2020-04-04 15:02:43 +0000

    about

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
   
 });
 
+app.get('/about', (req, res) => {
+  var fs = require("fs");
+  fs.readFile('about.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+  console.log('API was successfully requested');
+  
+});
+
 const server = app.listen(port, () => {
   console.log(runningMessage);
 });
